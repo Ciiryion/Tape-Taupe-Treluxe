704af4f18c12ec14e6db17fcd9d884d2c4db2c3b
fredv <fred.veron43@gmail.com>
2025-12-03 17:54:18 +0100

    Gestion vie du non appuie du joueur

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -40,13 +40,14 @@ public class PlaySound : MonoBehaviour
 
     private IEnumerator taupeManagement()
     {
-        //Il y a 75% de chances que le son soit une taupe
+        //Il y a 65% de chances que le son soit une taupe
         float rnd = Random.Range(0, 100);
-        if (rnd < 75)
+        if (rnd < 65)
             isTaupe = true;
         else
             isTaupe = false;
 
+        //Gestion du son
         if (isTaupe && canActive)
         {
             gameObject.GetComponent<AudioSource>().PlayOneShot(taupeSons[Random.Range(0, taupeSons.Length)]);
@@ -60,16 +61,22 @@ public class PlaySound : MonoBehaviour
             Debug.Log("isFeinte");
         }
 
-        // Interval de temps aleatoire avant le prochain son
+        //Interval de temps aleatoire avant le prochain son
         interval = Random.Range(intervalMin, intervalMax);
         Debug.Log("interval = " + interval);
         yield return new WaitForSeconds(interval);
+        //Le joueur perd une vie si la coroutine se termine sans qu'il ait appuyé sur le bouton
+        if (isTaupe && isActive)
+        {
+            gameManager.gameObject.GetComponent<GameManager>().lifeManagement();
+        }
+
+        //Si le joueur a perdu on ne relance pas la coroutine
         if (!gameManager.gameObject.GetComponent<GameManager>().getDefeat())
         {
             isActive = false;
             StartCoroutine(taupeManagement());
         }
-        
     }
 
     private IEnumerator taupeStart()
