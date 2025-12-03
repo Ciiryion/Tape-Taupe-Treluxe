056932dbd66a9b84a171cced6ef5eacbfc9f5634
Maxime Albertelli <91539486+Maxime-Albertelli@users.noreply.github.com>
2025-12-03 17:30:04 +0100

    Ajout de bruits à l'appui des boutons

--- Assets/Scripts/DebugButton.cs
+++ Assets/Scripts/DebugButton.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DebugButton : MonoBehaviour
 {
     [SerializeField] GameObject gameManager;
+    [SerializeField] private AudioSource[] audioSources;
     ArduinoConnector arduinoManager;
     String arduinoData;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +16,27 @@ public class DebugButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        arduinoData = arduinoManager.data;  
+        arduinoData = arduinoManager.data;
+        if (new string(arduinoData[11],1) == "1")
+        {
+            audioSources[0].Play();
+        }
+        if(new string(arduinoData[27], 1) == "1")
+        {
+            audioSources[1].Play();
+        }
+        if (new string(arduinoData[43], 1) == "1")
+        {
+            audioSources[2].Play();
+        }
+        if (new string(arduinoData[60], 1) == "1")
+        {
+            audioSources[3].Play();
+        }
+        if (new string(arduinoData[77], 1) == "1")
+        {
+            audioSources[4].Play();
+        }
         Debug.Log(arduinoData);
     }
 }
