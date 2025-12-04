48f992346c96693dfbf35844ae309544bc06f3bf
Maxime Albertelli <91539486+Maxime-Albertelli@users.noreply.github.com>
2025-12-04 10:28:24 +0100

    Relie l'interface avec la logique code (pv et score), timer pas encore fonctionnel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int vie, score;
+    int minutes, seconds;
+    float deltaTimer;
+    string niceTime;
     private bool defeat;
     //[SerializeField]
     //GameObject[] sonsPlace;
     [SerializeField]
     AudioClip validSound, wrongSound;
+    [SerializeField] TMP_Text scoreTxt, timerTxt;
+    [SerializeField] Image[] vieTab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +27,43 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Gestion du timer (non fonctionnel)
+        deltaTimer += Time.deltaTime;
+        minutes = Mathf.FloorToInt(Time.deltaTime / 60F);
+        seconds = Mathf.FloorToInt(Time.deltaTime - minutes * 60);
+        niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        timerTxt.text = niceTime;
+
+        //Affichage du score
+        scoreTxt.text = "Score : " + score;
+
+        //Gestion de l'UI des PV
+        for (int i = 0; i < vieTab.Length; i++)
+        {
+            vieTab[i].enabled = false;
+        }
+        switch (vie)
+        {
+            case 1:
+                for (int i = 0; i < vieTab.Length-2; i++)
+                {
+                    vieTab[i].enabled = true;
+                }
+                break;
+            case 2:
+                for (int i = 0; i < vieTab.Length-1; i++)
+                {
+                    vieTab[i].enabled = true;
+                }
+                break;
+            case 3:
+                for(int i = 0; i < vieTab.Length; i++)
+                {
+                    vieTab[i].enabled = true;
+                }
+                break;
+        }
+        
         if(defeat)
         {
             Debug.Log("C'est perdu");
